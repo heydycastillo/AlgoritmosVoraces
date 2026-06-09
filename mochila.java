1c875395ce02459b661c5018b98c705bc03368d6
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-09 18:54:29 -0500

    se puede ingresar los objetos y la capacidad

--- mochila.java
+++ mochila.java
@@ -1,8 +1,47 @@
+import java.util.Scanner;
+
 public class mochila {
 
     public static void main(String[] args) {
 
 
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.print("cantidad de objetos: ");
+        int cantidad = sc.nextInt();
+
+
+        String[] nombres = new String[cantidad];
+        double[] valores = new double[cantidad];
+        double[] pesos = new double[cantidad];
+
+
+        for(int i = 0; i < cantidad; i++){
+
+
+            System.out.println("objeto " + (i + 1));
+
+
+            System.out.print("nombre: ");
+            nombres[i] = sc.next();
+
+
+            System.out.print("valor: ");
+            valores[i] = sc.nextDouble();
+
+
+            System.out.print("peso: ");
+            pesos[i] = sc.nextDouble();
+
+        }
+
+
+        System.out.print("capacidad mochila: ");
+        double capacidad = sc.nextDouble();
+
+
+        sc.close();
 
     }
 
