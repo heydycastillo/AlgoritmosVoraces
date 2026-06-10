ec5c356301622487f0e92c7001171bf31f3bb5a0
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-09 19:21:45 -0500

    implemento de orden greedy y de seleccion de objetos

--- mochila.java
+++ mochila.java
@@ -8,6 +8,7 @@ public class mochila {
         Scanner sc = new Scanner(System.in);
 
 
+        // ingreso de cantidad de objetos
         System.out.print("cantidad de objetos: ");
         int cantidad = sc.nextInt();
 
@@ -17,10 +18,11 @@ public class mochila {
         double[] pesos = new double[cantidad];
 
 
+        // ingreso de datos de cada objeto
         for(int i = 0; i < cantidad; i++){
 
 
-            System.out.println("objeto " + (i + 1));
+            System.out.println("\nobjeto " + (i + 1));
 
 
             System.out.print("nombre: ");
@@ -37,8 +39,122 @@ public class mochila {
         }
 
 
-        System.out.print("capacidad mochila: ");
+        System.out.print("\ncapacidad mochila: ");
         double capacidad = sc.nextDouble();
+
+
+
+        // calcular valor/peso
+        double[] ratios = new double[cantidad];
+
+
+        for(int i = 0; i < cantidad; i++){
+
+            ratios[i] = valores[i] / pesos[i];
+
+        }
+
+
+
+        // ordenar de mayor a menor ratio (Greedy)
+
+        for(int i = 0; i < cantidad - 1; i++){
+
+
+            for(int j = 0; j < cantidad - i - 1; j++){
+
+
+                if(ratios[j] < ratios[j + 1]){
+
+
+                    // cambiar ratios
+                    double auxRatio = ratios[j];
+
+                    ratios[j] = ratios[j + 1];
+
+                    ratios[j + 1] = auxRatio;
+
+
+
+                    // cambiar valores
+
+                    double auxValor = valores[j];
+
+                    valores[j] = valores[j + 1];
+
+                    valores[j + 1] = auxValor;
+
+
+
+                    // cambiar pesos
+
+                    double auxPeso = pesos[j];
+
+                    pesos[j] = pesos[j + 1];
+
+                    pesos[j + 1] = auxPeso;
+
+
+
+                    // cambiar nombres
+
+                    String auxNombre = nombres[j];
+
+                    nombres[j] = nombres[j + 1];
+
+                    nombres[j + 1] = auxNombre;
+
+
+                }
+
+            }
+
+        }
+
+
+
+        // seleccion de objetos completos
+
+        double valorTotal = 0;
+
+        double restante = capacidad;
+
+
+
+        System.out.println("\nobjetos seleccionados:");
+
+
+
+        for(int i = 0; i < cantidad; i++){
+
+
+
+            if(pesos[i] <= restante){
+
+
+
+                restante -= pesos[i];
+
+                valorTotal += valores[i];
+
+
+
+                System.out.println(
+                    nombres[i] + " completo"
+                );
+
+
+            }
+
+        }
+
+
+
+        System.out.println(
+            "\nvalor total aproximado: " +
+            (int)valorTotal
+        );
+
 
 
         sc.close();
