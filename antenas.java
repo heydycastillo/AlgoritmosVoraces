937dff81984a33bcebe3a6503d6ae1e79c7abb65
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-09 19:41:52 -0500

    se puede ingresar casas y rango de cobertura

--- antenas.java
+++ antenas.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class antenas {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("cantidad de casas: ");
+        int cantidad = sc.nextInt();
+
+        int[] casas = new int[cantidad];
+
+        for(int i = 0; i < cantidad; i++){
+
+            System.out.print("posicion casa " + (i + 1) + ": ");
+            casas[i] = sc.nextInt();
+
+        }
+
+        System.out.print("rango antena: ");
+        int rango = sc.nextInt();
+
+        sc.close();
+
     }
+
 }
