1f3ed4926fb288c9a90f1a01db1b5f97bdcb5a09
heydy <hcastilloa@ulasalle.edu.pe>
2026-06-09 20:09:02 -0500

    se muestra cantidad de antenas mostradas

--- antenas.java
+++ antenas.java
@@ -14,10 +14,8 @@ public class antenas {
         int[] casas = new int[cantidad];
 
         for (int i = 0; i < cantidad; i++) {
-
             System.out.print("posicion casa " + (i + 1) + ": ");
             casas[i] = sc.nextInt();
-
         }
 
         System.out.print("rango antena: ");
@@ -25,23 +23,32 @@ public class antenas {
 
         sc.close();
 
-        // ORDENAMIENTO (hasta aquí tu commit 3)
-
         for (int i = 0; i < cantidad - 1; i++) {
-
             for (int j = 0; j < cantidad - i - 1; j++) {
-
                 if (casas[j] > casas[j + 1]) {
-
                     int aux = casas[j];
                     casas[j] = casas[j + 1];
                     casas[j + 1] = aux;
-
                 }
-
             }
-
         }
 
+        int i = 0;
+        int total = 0;
+
+        System.out.println("\nantenas colocadas en:");
+
+        while (i < cantidad) {
+            int antena = casas[i] + rango;
+            System.out.println(antena);
+            total++;
+            int cobertura = antena + rango;
+
+            while (i < cantidad && casas[i] <= cobertura) {
+                i++;
+            }
+        }
+
+        System.out.println("cantidad total de antenas: " + total);
     }
 }
